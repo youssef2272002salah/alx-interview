d8480803c8b6d7e8a78116308ba447a8e8deaa1c
youssef2272002salah <youssef22772002salah@gmail.com>
2024-08-07 22:06:53 +0300

    1

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -17,7 +17,7 @@ def isWinner(x, nums):
 
         isMariaTurns = True
 
-        while(True):
+        while (True):
             if not primesSet:
                 if isMariaTurns:
                     benWinsCount += 1
@@ -53,5 +53,5 @@ def is_prime(n):
 
 def primes_in_range(start, end):
     """Returns a list of prime numbers between start and end (inclusive)."""
-    primes = [n for n in range(start, end+1) if is_prime(n)]
+    primes = [n for n in range(start, end + 1) if is_prime(n)]
     return primes
